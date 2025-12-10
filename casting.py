db3bc1ff8437a293cbef83a7f874cd3e25790f45
putradev <wardiputra94@gmail.com>
2025-12-10 13:19:45 +0700

    casting in python

---
+++ casting.py
@@ -0,0 +1,47 @@
+print("====int====")
+
+integer = 1
+
+string = str(integer)
+floatt = float(integer)
+boolean = bool(integer)
+
+print("string=", string, "tipe data=", type(string))
+print("float=", floatt, "tipe data=", type(floatt))
+print("boolean=", boolean, "tipe data=", type(boolean))
+
+print("====str====")
+
+string = "10"
+
+integer = int(string)
+floatt = float(string)
+boolean= bool(string)
+
+print("integer=", integer, "tipe data=", type(integer))
+print("float=", floatt, "tipe data=", type(floatt))
+print("boolean=", boolean, "tipe data=", type(boolean))
+
+print("====float====")
+
+floatt = 10.4
+
+integer = int(floatt)
+string = str(floatt)
+boolean= bool(floatt)
+
+print("integer=", integer, "tipe data=", type(integer))
+print("string=", string, "tipe data=", type(string))
+print("boolean=", boolean, "tipe data=", type(boolean))
+
+print("====boolean====")
+
+boolean = False
+
+integer = int(boolean)
+string = str(boolean)
+floatt= float(boolean)
+
+print("integer=", integer, "tipe data=", type(integer))
+print("string=", string, "tipe data=", type(string))
+print("float=", floatt, "tipe data=", type(floatt))
